d1185d980deda934fb9827a03a38a5b3b2b041dd
Ex3lin <exelinger@yandex.ru>
2024-03-27 18:09:42 +0700

    ch

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 { 
     use HasFactory;
     protected $table = 'tags';
     protected $fillable = ['name'];
+
+    public function invoiceTags(): BelongsToMany{
+        return $this->belongsToMany(Invoice::class,'invoice_tag','tag_id','invoice_id');
+    }
 }
